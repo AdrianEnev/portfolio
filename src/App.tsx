715c4ed3ad90ffe1e6472565e9a2599a7de63290
Adrian <enevadrian@gmail.com>
2025-05-07 21:01:50 +0300

    Initialized vite,react,tailwind4,wouter app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className='m-3'>
+                <p className='text-xl font-medium text-red-500 text-center'>Hi!</p>
+            </div>
+        </>
+    )
+}
+
+export default App
